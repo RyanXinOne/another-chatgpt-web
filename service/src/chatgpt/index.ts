194be0ea6e2b1e746f3501e6eae51ecf3263dd1f
Ryan Xin <xyz@ryanxin.cn>
2024-06-28 16:53:26 +0100

    refactor: last commit

--- service/src/chatgpt/index.ts
+++ service/src/chatgpt/index.ts
@@ -1,5 +1,6 @@
 import * as dotenv from 'dotenv'
 import OpenAI from 'openai'
+import type { CompletionUsage } from 'openai/src/resources/completions'
 import { encoding_for_model } from 'tiktoken'
 import type { TiktokenModel } from 'tiktoken'
 import { sendResponse } from '../utils'
@@ -15,7 +16,7 @@ const DEBUG_MODE = process.env.DEBUG_MODE === 'true'
 if (!isNotEmptyString(process.env.OPENAI_API_KEY))
   throw new Error('Missing OPENAI_API_KEY environment variable')
 
-function filterMessagesByTokenCount(messages: Message[], model: string, max_tokens?: number): Message[] {
+function filterMessagesByTokenCount(messages: Message[], model: string, max_tokens?: number): { messages: Message[]; estimated_tokens: number } {
   const encoding = encoding_for_model(model as TiktokenModel)
   const tokens_per_message = 3
   const count_message_token = (message: Message) => {
@@ -25,26 +26,22 @@ function filterMessagesByTokenCount(messages: Message[], model: string, max_toke
     }
     return tokens
   }
-  let total_tokens = 3
+  let estimated_tokens = 3
 
   if (messages.length > 0 && messages[0].role === 'system') {
-    total_tokens += count_message_token(messages[0])
+    estimated_tokens += count_message_token(messages[0])
   }
 
   for (let i = messages.length - 1; i >= 1; i--) {
     let curr_tokens = count_message_token(messages[i])
-    if (max_tokens && total_tokens + curr_tokens > max_tokens) {
+    if (max_tokens && estimated_tokens + curr_tokens > max_tokens) {
       messages.splice(1, i)
       break
     }
-    total_tokens += curr_tokens
+    estimated_tokens += curr_tokens
   }
 
-  if (DEBUG_MODE) {
-    global.console.log(`Estimated tokens: ${total_tokens}`)
-    global.console.log(`Messages: ${JSON.stringify(messages, null, 2)}`)
-  }
-  return messages
+  return { messages, estimated_tokens }
 }
 
 const openai = new OpenAI({
@@ -53,14 +50,18 @@ const openai = new OpenAI({
 
 export async function chatReplyProcess(options) {
   let { model, messages, temperature, top_p, callback } = options as RequestOptions
+  let estimated_tokens: number
+  ({ messages, estimated_tokens } = filterMessagesByTokenCount(messages, model, MAX_INPUT_TOKENS))
   if (DEBUG_MODE) {
     global.console.log('-'.repeat(30))
     global.console.log(`Time: ${new Date().toISOString()}`)
     global.console.log(`Model: ${model}`)
     global.console.log(`Temperature: ${temperature}`)
     global.console.log(`Top P: ${top_p}`)
+    global.console.log(`Estimated tokens: ${estimated_tokens}`)
+    global.console.log(`Messages: ${JSON.stringify(messages, null, 2)}`)
   }
-  messages = filterMessagesByTokenCount(messages, model, MAX_INPUT_TOKENS)
+  let usage: CompletionUsage | undefined
   try {
     const stream = await openai.chat.completions.create({
       model,
@@ -73,12 +74,13 @@ export async function chatReplyProcess(options) {
     })
     for await (const chunk of stream) {
       if (chunk.usage) {
-        if (DEBUG_MODE) {
-          global.console.log(`Usage: ${JSON.stringify(chunk.usage, null, 2)}`)
-        }
+        usage = chunk.usage
         break
       }
       callback(chunk)
+    }
+    if (DEBUG_MODE) {
+      global.console.log(`Usage: ${JSON.stringify(usage, null, 2)}`)
     }
     return sendResponse({ type: 'Success' })
   } catch (error: any) {
